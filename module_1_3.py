96739ae5e235c0ed95da127d9eddb54fc70e7cda
GorodGood <148943341+GorodGood@users.noreply.github.com>
2024-08-29 21:38:45 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,13 @@
+name="Iwan"
+print(name, type(name))
+
+Age= 33
+print(Age, type(Age))
+
+New_Age= 34
+print(New_Age, type(New_Age))
+
+print(Age + New_Age,type(Age + New_Age))
+
+is_student='True'
+print(is_student, type[True])
