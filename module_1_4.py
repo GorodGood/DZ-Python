cfb821da3c731e8244730b09b119da80fe3e7c97
GorodGood <148943341+GorodGood@users.noreply.github.com>
2024-08-31 14:14:28 +0300

    Add files via upload

--- module_1_4.py
+++ module_1_4.py
@@ -1,12 +1,13 @@
 #Организуйте_программу
-from os import replace
 
 my_string= input("В каком городе вы живете? ")
+print(len(my_string))
 
 #Работа_с_методами_строк
-print(("В каком городе вы живете?".replace("живете", "")).upper())
-print(("В каком городе вы живете?".replace("каком", "")).lower())
-print("В каком городе вы живете?".replace(" ", ""))
+my_string=("В каком городе вы живете?")
+print((my_string .replace("живете", "")).upper())
+print((my_string .replace("каком", "")).lower())
+print(my_string  .replace(" ", ""))
 print(my_string[0])
 print(str(my_string[-1]))
 
